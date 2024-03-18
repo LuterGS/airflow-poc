83318d4b84cf9f203e3da25739d8a2b85b85b362
LuterGS <koo04034@gmail.com>
2024-03-18 10:39:48 +0900

    add sample JAR dag

---
+++ jar_run.py
@@ -0,0 +1,39 @@
+from datetime import timedelta, datetime
+
+from airflow.decorators import dag, task
+from airflow.hooks.base import BaseHook
+
+
+@dag(
+    dag_id="jar_run",
+    default_args={
+        "depends_on_past": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+    },
+    description="sample JAR run DAG",
+    schedule=timedelta(minutes=10),
+    start_date=datetime(2024, 3, 18),
+    catchup=False,
+    tags=["smartcity"]
+)
+def operator():
+    @task(task_id="read_jar_location")
+    def read_jar_location():
+        conn = BaseHook.get_connection("jar_loc")
+        conn.get_extra()
+        print(conn)
+
+    read_jar_loc = read_jar_location()
+
+    @task(task_id="print_test")
+    def print_test():
+        print("test")
+
+    print_t = print_test()
+
+
+    read_jar_loc >> print_t
+
+
+operator()
